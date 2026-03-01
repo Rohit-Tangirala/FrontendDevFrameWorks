23cf7053c2fd2cb4ff8880461288331a3ba8c909
Rohit-Tangirala <tgssrohit2306@gmail.com>
2026-03-01 20:00:49 +0530

    Skilling -07
    Spring Boot Model View Controller Architecture

---
+++ springmvc-crud/src/test/java/com/spring/springmvc_crud/SpringmvcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springmvc_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
